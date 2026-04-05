f845b3979ef5d151a8d2f42af7109faee34e342f
Carlos Carbajal <carlos.carbajal.d@tecsup.edu.pe>
2026-04-05 18:02:17 -0500

    estructura base sprint-1

---
+++ src/main/java/pe/edu/tecsup/lab03/entities/StudentEntity.java
@@ -0,0 +1,14 @@
+package pe.edu.tecsup.lab03.entities;
+
+public class StudentEntity {
+
+    private int id;
+    private String name;
+
+    public StudentEntity() {}
+
+    public StudentEntity(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
